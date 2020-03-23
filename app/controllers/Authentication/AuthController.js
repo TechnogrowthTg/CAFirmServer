fe1de96f7c4fa2b3a93d2d8bf9b2beb3ed37292b
amollogipool <amol.logipool@gmail.com>
2020-03-23 10:52:35 +0530

    auth changes

--- app/controllers/Authentication/AuthController.js
+++ app/controllers/Authentication/AuthController.js
@@ -1,7 +1,7 @@
 var mysqlQuery = require("../../common/mysqlConnection");
 var jwt = require("jsonwebtoken"); // used to create, sign, and verify tokens
 var config = require("../../../config/config");
-var dateFormat = require("dateformat");
+// var dateFormat = require("dateformat");
 
 /**
  * This function represent user Login
@@ -11,25 +11,25 @@ var dateFormat = require("dateformat");
  */
 function userLogin(req, res) {
   var param = req.body;
-  var now = new Date();
-  var current_date = dateFormat(now, "isoDate");
+  // var now = new Date();
+  // var current_date = dateFormat(now, "isoDate");
   var query = "SELECT * FROM user WHERE UserEmail='" + param.email + "' AND Password='" + param.password + "' AND IsDeleted=1";
   mysqlQuery.excecuteQuery(query, function (error, result) {
-    if (error)
+    if (error) {
       return res.json({
         error: true,
         message: error
       })
-
+    }
     if (result.length > 0) {
 
       var payload = {
         UserId: result[0].UserId,
-        RoleId: result[0].RoleId,
-        UserName: result[0].UserName,
+        // RoleId: result[0].RoleId,
+        // UserName: result[0].UserName,
         UserEmail: result[0].UserEmail,
         Password: result[0].Password,
-        IsDeleted: result[0].IsDeleted
+        // IsDeleted: result[0].IsDeleted
       }
 
       var token = jwt.sign(payload, config.secret, { expiresIn: 28800 });
